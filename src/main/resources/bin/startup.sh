a227f47b53853272f063c5419857e2d063847abf
CarlosXiao <carlosxiao@dr.com>
2018-08-05 17:14:35 +0800

    add custom parameter inject.

--- src/main/resources/bin/startup.sh
+++ src/main/resources/bin/startup.sh
@@ -1,11 +1,19 @@
 #!/usr/bin/env bash
 
+##
+# usage: startup.sh prod 25 1 1560
+##
+
 #startup for application
 ACTIVE_PROFILE=$1
 if [ -z $1 ]; then
     echo "active environment not specified use default: dev"
     ACTIVE_PROFILE="dev"
 fi
+
+PAGE_SIZE=$2
+START_PAGE=$3
+TOTAL_PAGE=$4
 
 if [ -z $JAVA_HOME ]; then
   echo "ERROR: JAVA_HOME is not found in your Environment."
@@ -30,4 +38,4 @@ MAX_HEAP=2G
 
 JAVA_OPTS="-server -Xmx$MAX_HEAP -Xms$MAX_HEAP -Xmn256m -Xss512k -XX:+DisableExplicitGC -XX:+UseConcMarkSweepGC -XX:+CMSParallelRemarkEnabled -XX:LargePageSizeInBytes=128m -XX:+UseFastAccessorMethods -XX:+UseCMSInitiatingOccupancyOnly -XX:CMSInitiatingOccupancyFraction=70 -Duser.timezone=GMT+8"
 ## -Detherscan.startPage=1
-nohup $JAVA_HOME/bin/java -DAPP_HOME=$APP_HOME -Dspring.profiles.active=$ACTIVE_PROFILE -cp $CP $JAVA_OPTS -Xloggc:$APP_HOME/logs/gc.log org.springframework.boot.loader.JarLauncher --server.tomcat.basedir=$APP_HOME/logs --server.tomcat.access-log-enabled=true --server.tomcat.accesslog.directory=$APP_HOME/logs --server.tomcat.access-log-pattern='%{X-FORWARDED-FOR}i %l %u %t "%r" %s %b %D %q "%{User-Agent}i" %T' > /dev/null 2>&1 &
+nohup $JAVA_HOME/bin/java -DAPP_HOME=$APP_HOME -Dspring.profiles.active=$ACTIVE_PROFILE -Detherscan.startPage=$START_PAGE -Detherscan.pageSize=$PAGE_SIZE -Detherscan.totalPage=$TOTAL_PAGE -cp $CP $JAVA_OPTS -Xloggc:$APP_HOME/logs/gc.log org.springframework.boot.loader.JarLauncher --server.tomcat.basedir=$APP_HOME/logs --server.tomcat.access-log-enabled=true --server.tomcat.accesslog.directory=$APP_HOME/logs --server.tomcat.access-log-pattern='%{X-FORWARDED-FOR}i %l %u %t "%r" %s %b %D %q "%{User-Agent}i" %T' > /dev/null 2>&1 &
